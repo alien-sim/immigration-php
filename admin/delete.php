7081eb7dedee0eb1be04110b57b09fc3dd2fc342
simran <simrajput555@gmail.com>
2020-06-14 19:22:38 +0530

    delete file updated

--- admin/delete.php
+++ admin/delete.php
@@ -3,10 +3,14 @@
 	include_once './config.php';
 	$id = $_GET['id'];
 	$page = $_GET['page']; 
-	echo $id;
-	echo $page;
-	$sql="delete from ".$page." where id='".$id."'";
-	// $q=mysqli_query($db,$sql);
+	if($page == 'agents'){
+		$table = 'admin';
+	}
+	else{
+		$table = $_GET['page'];
+	}
+
+	$sql="delete from ".$table." where id='".$id."'";
 	if(mysqli_query($db,$sql)){
 		$success="successfully Deleted";
 		header("location:".$page.".php?success=$success");
